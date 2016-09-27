95e83ede840b6bff40a9f9027fd6b39899558e3e
pablo lopez <pablo lopez>
2016-09-27 19:30:52 +0200

    1er commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+  </head> 
+   <body>
+      <?php
+        echo "Pablo ";
+        echo "Lopez ";
+        print "Sanchis";
+      ?>
+    </body>
+</html>
